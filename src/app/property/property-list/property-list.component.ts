fb145186c2fc3098cb268d79a03b5b83a1295063
KALASA_IVAN <moikalasa12@gmail.com>
2022-04-22 12:02:16 +0200

    property list

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -3,13 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
+  Property: Array<any> = [
+    {
+      Id: 1,
+      Name: 'Karkis',
+      Type: 'House',
+      Price: 12000,
+    },
+    {
+      Id: 2,
+      Name: 'Max Square Garden',
+      Type: 'House',
+      Price: 100000,
+    },
+    {
+      Id: 3,
+      Name: 'KI Hall',
+      Type: 'House',
+      Price: 20000,
+    },
+    {
+      Id: 4,
+      Name: 'Union TR',
+      Type: 'House',
+      Price: 14000,
+    },
+    {
+      Id: 5,
+      Name: 'Ucumi Inc',
+      Type: 'House',
+      Price: 26000,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
