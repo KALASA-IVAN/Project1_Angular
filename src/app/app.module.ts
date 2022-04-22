5e09edf1e7131f82a07e025696289fa1b8ba49b7
KALASA_IVAN <moikalasa12@gmail.com>
2022-04-22 08:49:59 +0200

    Created two more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
-
+import { PropertyListComponent } from './property/property-list/property-list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PropertyCardComponent
+    PropertyCardComponent,
+    PropertyListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
